3609247d5bae93eae0d1161bec0c030a4337c344
melektron <matteo@elektron.work>
2022-12-09 01:23:07 +0100

    added conversion from json to universal

--- include/el/conversions/json.hpp
+++ include/el/conversions/json.hpp
@@ -16,12 +16,13 @@ which must be includable like this: "#include <nlohmann-json/json.hpp>"
 
 #include <nlohmann-json/json.hpp>
 #include "../universal.hpp"
+#include "../jsonutils.hpp"
 
 
 
 namespace el
 {
-    nlohmann::json universal_to_json(const universal &_data)
+    inline nlohmann::json universal_to_json(const universal &_data)
     {
         if (_data.get_type() == universal::type_t::string)
             return _data.to_string();
@@ -41,5 +42,36 @@ namespace el
             };
         }
         return {};
+    }
+
+    inline universal universal_from_json(const nlohmann::json &_data)
+    {
+        json_type_t type = _data.type();
+        switch (type)
+        {
+        case json_type_t::string:
+            return universal(_data.get<std::string>());
+        case json_type_t::number_integer:
+        case json_type_t::number_unsigned:
+            return universal(_data.get<int64_t>());
+        case json_type_t::number_float:
+            return universal(_data.get<double>());
+        case json_type_t::boolean:
+            return universal(_data.get<bool>());
+        case json_type_t::object:
+        {
+            if (json_validate(_data, "r", json_type_t::number_unsigned) &&
+                json_validate(_data, "g", json_type_t::number_unsigned) &&
+                json_validate(_data, "b", json_type_t::number_unsigned))
+                return universal(types::rgb24_t(
+                    _data.at("r").get<uint8_t>(),
+                    _data.at("g").get<uint8_t>(),
+                    _data.at("b").get<uint8_t>()));
+            // fallthrough to default if not valid object
+        }
+        
+        default:
+            return universal();
+        }
     }
 };
