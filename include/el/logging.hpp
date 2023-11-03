9f2413c348afff66cf43bfa284a5174e45da7afe
melektron <matteo@elektron.work>
2023-11-03 16:34:00 +0100

    updated strutil and specifically format and improved logging

--- include/el/logging.hpp
+++ include/el/logging.hpp
@@ -46,7 +46,9 @@ Simple logging framework
 #define EL_LOGW(fmt, ...) logger_inst.warning(__LINE__, fmt, ## __VA_ARGS__)
 #define EL_LOGI(fmt, ...) logger_inst.info(__LINE__, fmt, ## __VA_ARGS__)
 #define EL_LOGD(fmt, ...) logger_inst.debug(__LINE__, fmt, ## __VA_ARGS__)
-#define EL_LOG_EXCEPTION(ex) EL_LOGE("Exception occured: %s", el::logging::format_exception(ex).c_str())
+
+#define EL_LOG_EXCEPTION_MSG(msg, ex) EL_LOGE(msg ": %s", el::logging::format_exception(ex).c_str())
+#define EL_LOG_EXCEPTION(ex) EL_LOG_EXCEPTION_MSG("Exception occurred", ex)
 
 namespace el::logging
 {
@@ -74,76 +76,103 @@ namespace el::logging
         logger(std::string _file_name)
             : m_file_name(_file_name)
         {}
+        
+        // Critical
 
-        template<typename... _Args>
-        void critical(int _line, const std::string _fmt, _Args... _args)
+        void critical(int _line, const std::string &_message)
         {
-            // format the message
-            const std::string message = strutil::format(_fmt, _args...);
-
             // generate prefix
             char prefix_buffer[_EL_LOG_PREFIX_BUFFER_SIZE];
             generate_prefix(prefix_buffer, _line, "C");
 
-            // print in red
-            std::cout << _EL_LOG_ANSI_COLOR_RED << prefix_buffer << message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
+            // print in color
+            std::cout << _EL_LOG_ANSI_COLOR_RED << prefix_buffer << _message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
         }
 
         template<typename... _Args>
-        void error(int _line, const std::string _fmt, _Args... _args)
+        void critical(int _line, const std::string &_fmt, _Args... _args)
         {
             // format the message
-            const std::string message = strutil::format(_fmt, _args...);
+            critical(_line, strutil::format(_fmt, _args...));
+        }
 
+        // Error
+
+        void error(int _line, const std::string &_message)
+        {
             // generate prefix
             char prefix_buffer[_EL_LOG_PREFIX_BUFFER_SIZE];
             generate_prefix(prefix_buffer, _line, "E");
 
-            // print in red
-            std::cout << _EL_LOG_ANSI_COLOR_RED << prefix_buffer << message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
+            // print in color
+            std::cout << _EL_LOG_ANSI_COLOR_RED << prefix_buffer << _message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
         }
 
         template<typename... _Args>
-        void warning(int _line, const std::string _fmt, _Args... _args)
+        void error(int _line, const std::string &_fmt, _Args... _args)
         {
             // format the message
-            const std::string message = strutil::format(_fmt, _args...);
+            error(_line, strutil::format(_fmt, _args...));
+        }
 
+
+        // Warning
+
+        void warning(int _line, const std::string &_message)
+        {
             // generate prefix
             char prefix_buffer[_EL_LOG_PREFIX_BUFFER_SIZE];
             generate_prefix(prefix_buffer, _line, "W");
 
-            // print in red
-            std::cout << _EL_LOG_ANSI_COLOR_YELLOW << prefix_buffer << message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
+            // print in color
+            std::cout << _EL_LOG_ANSI_COLOR_YELLOW << prefix_buffer << _message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
         }
 
         template<typename... _Args>
-        void info(int _line, const std::string _fmt, _Args... _args)
+        void warning(int _line, const std::string &_fmt, _Args... _args)
         {
             // format the message
-            const std::string message = strutil::format(_fmt, _args...);
+            warning(_line, strutil::format(_fmt, _args...));
+        }
 
+        // Info
+
+        void info(int _line, const std::string &_message)
+        {
             // generate prefix
             char prefix_buffer[_EL_LOG_PREFIX_BUFFER_SIZE];
-            generate_prefix(prefix_buffer, _line, "W");
+            generate_prefix(prefix_buffer, _line, "I");
 
-            // print in red
-            std::cout << _EL_LOG_ANSI_COLOR_RESET << prefix_buffer << message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
+            // print in color
+            std::cout << _EL_LOG_ANSI_COLOR_RESET << prefix_buffer << _message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
         }
 
         template<typename... _Args>
-        void debug(int _line, const std::string _fmt, _Args... _args)
+        void info(int _line, const std::string &_fmt, _Args... _args)
         {
             // format the message
-            const std::string message = strutil::format(_fmt, _args...);
+            info(_line, strutil::format(_fmt, _args...));
+        }
 
+        // Debug
+
+        void debug(int _line, const std::string &_message)
+        {
             // generate prefix
             char prefix_buffer[_EL_LOG_PREFIX_BUFFER_SIZE];
-            generate_prefix(prefix_buffer, _line, "W");
+            generate_prefix(prefix_buffer, _line, "D");
 
-            // print in red
-            std::cout << _EL_LOG_ANSI_COLOR_GREEN << prefix_buffer << message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
+            // print in color
+            std::cout << _EL_LOG_ANSI_COLOR_GREEN << prefix_buffer << _message << _EL_LOG_ANSI_COLOR_RESET << std::endl;
         }
+
+        template<typename... _Args>
+        void debug(int _line, const std::string &_fmt, _Args... _args)
+        {
+            // format the message
+            debug(_line, strutil::format(_fmt, _args...));
+        }
+        
     };
 
     /**
--- include/el/strutil.hpp
+++ include/el/strutil.hpp
@@ -29,35 +29,36 @@ namespace el::strutil
      * NOTE: This method uses dynamic memory allocation ("new" operator) and std::unique_ptr.
      * Usually, the template arguments don't have to be provided but can be deducted from function
      * arguments.
+     * Inspiration: https://stackoverflow.com/a/26221725
      * 
-     * @tparam _ST string type of the format and return value. The type must be constructable
-     * from "const char *" (string copy) and must have a .c_str() method to convert to "char *"".
      * @tparam _Args varadic format argument types
      * @param _fmt Format string
      * @param _args Format arguments
-     * @return _ST newly created string of specified type
+     * @return newly created string of specified type
      */
-    template<typename _ST, typename... _Args>
-    _ST format(const _ST& _fmt, _Args... _args)
+    template<typename... _Args>
+    std::string format(const std::string& _fmt, _Args... _args)
     {
-        size_t len = snprintf(nullptr, 0, _fmt.c_str(), _args...) + 1;  // extra space for null byte
+        int len_or_error = std::snprintf(nullptr, 0, _fmt.c_str(), _args...) + 1;  // extra space for null byte
+        
+        if( len_or_error <= 0 )
+            throw std::runtime_error( "Error during formatting." );
+        auto len = static_cast<size_t>(len_or_error);
+        
         std::unique_ptr<char[]> _cstr(new char[len]);
-        snprintf(_cstr.get(), len, _fmt.c_str(), _args...);
-        return _ST(_cstr.get());
+        std::snprintf(_cstr.get(), len, _fmt.c_str(), _args...);
+        
+        return std::string(_cstr.get());
     }
 
     /**
      * @brief creates a copy of a string with all lowercase letters.
      * The tolower() C function is used to convert the letters. 
-     * Any string class compatible with the C++ std::string class in terms
-     * of iteration and uses "char" as the character type can be used.
      * 
-     * @tparam _ST string type to be used (deducted, typically std::string)
      * @param instr the input string to convert
-     * @return _ST copy of the string in lowercase
+     * @return copy of the string in lowercase
      */
-    template<typename _ST>
-    _ST lowercase(_ST instr)
+    std::string lowercase(std::string instr)
     {
         std::for_each(instr.begin(), instr.end(), [](char &c)
                     { c = ::tolower(c); });
@@ -68,15 +69,11 @@ namespace el::strutil
     /**
      * @brief creates a copy of a string with all lowercase letters.
      * The tolower() C function is used to convert the letters. 
-     * Any string class compatible with the C++ std::string class in terms
-     * of iteration and uses "char" as the character type can be used.
      * 
-     * @tparam _ST string type to be used (deducted, typically std::string)
      * @param instr the input string to convert
-     * @return _ST copy of the string in lowercase
+     * @return copy of the string in lowercase
      */
-    template<typename _ST>
-    _ST uppercase(_ST instr)
+    std::string uppercase(std::string instr)
     {
         std::for_each(instr.begin(), instr.end(), [](char &c)
                     { c = ::toupper(c); });
@@ -88,13 +85,11 @@ namespace el::strutil
      * @brief Reads the entire content of a file and stores it in a string.
      * @exception This function can trough any exception that the string or ifstream can.
      * 
-     * @tparam _ST string type, typically std::string (can be deducted)
      * @param _file The file stream to read from
      * @param _string The string to store the file contents in. This will overwrite the string.
      * @return The length of the file (= the number of characters copied to the string)
      */
-    template<typename _ST>
-    size_t read_file_into_string(std::ifstream &_file, _ST &_string)
+    size_t read_file_into_string(std::ifstream &_file, std::string &_string)
     {
         // get file length
         _file.seekg(0, std::ios::end);
