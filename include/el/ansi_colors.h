617346d4cd789af2bbe48cfe352ec070f84db816
melektron <matteo@elektron.work>
2025-02-07 15:36:16 +0100

    added some ansi color definitions

---
+++ include/el/ansi_colors.h
@@ -0,0 +1,20 @@
+/*
+ELEKTRON © 2025 - now
+Written by melektron
+www.elektron.work
+07.02.25, 15:34
+All rights reserved.
+
+This source code is licensed under the Apache-2.0 license found in the
+LICENSE file in the root directory of this source tree. 
+
+Some definitions of ANSI-ESC strings for terminal coloring.
+*/
+
+#pragma once
+
+#define EL_ANSI_RED "\e[31m"
+#define EL_ANSI_GREEN "\e[32m"
+#define EL_ANSI_YELLOW "\e[33m"
+#define EL_ANSI_BLUE "\e[34m"
+#define EL_ANSI_RESET "\e[0m"
