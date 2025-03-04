a2b8d7cf5a442289d47403ca850a6242209a3be9
melektron <matteo@elektron.work>
2025-03-04 16:28:09 +0100

    added assert return macros

--- include/el/macros.hpp
+++ include/el/macros.hpp
@@ -19,3 +19,8 @@ Little utility macros that really should be part of the C++ and/or C standard li
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+// returns something if the assertion fails
+#define EL_VRET_ASSERT(test, val) if !(test) return (val);
+// returns false if the assertion fails
+#define EL_NRET_ASSERT(test) if (!(test)) return false;
